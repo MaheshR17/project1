3140a7c99cbcd260d33346a546a6ba087d860a8f
Mahesh <rmaheshr77@gmail.com>
2021-07-30 12:36:33 +0530

    first commit

---
+++ src/main/java/com/javaexpress/demo/Application.java
@@ -0,0 +1,17 @@
+package com.javaexpress.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.javaexoress"})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Maheshwarayya");
+		
+	}
+
+}
---
+++ src/main/java/com/javaexpress/model/Model.java
@@ -0,0 +1,15 @@
+package com.javaexpress.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Model {
+	
+	
+	public Model() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Model Constructor");
+	}
+
+}
